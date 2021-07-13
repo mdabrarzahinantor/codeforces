36651304bf28b869d76340b3a05da7aa99b4f7fb
mdabrarzahinantor <mdabrarzahinantor2005@gmail.com>
2021-07-13 15:56:40 +0600

    first commit

---
+++ C++/Flea.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int number;
+
+    cout << "Enter an integer: ";
+    cin >> number;
+
+    cout << "You Entered: " << number;
+
+    return 0;
+}
---
+++ Python/Flea.py
@@ -0,0 +1,19 @@
+a = input()
+b = input()
+
+a = a.upper()
+b = b.upper()
+
+c = 0
+
+for i in range(len(a)):
+    if(a[i] < b[i]):
+        c = -1
+        break
+
+    elif(a[i] > b[i]):
+        c = 1
+        break
+
+
+print(c)
