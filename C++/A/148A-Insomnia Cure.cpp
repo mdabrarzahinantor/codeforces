ae7df3fcb680de64bf6b199838c1c30da892b629
mdabrarzahinantor <mdabrarzahinantor2005@gmail.com>
2021-07-22 18:24:18 +0600

    init

---
+++ C++/A/148A-Insomnia Cure.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int k, l, m, n, d, counter = 0;
+    cin >> k >> l >> m >> n >> d;
+
+    for (int i = 1; i <= d; i++)
+    {
+        if (i % k == 0 || i % l == 0 || i % m == 0 || i % n == 0)
+        {
+            counter++;
+        }
+    }
+    cout << counter;
+
+    return 0;
+}
